9244c470635017f37fc2c9b6fe2b344da4d19a51
JinalPanchal-29 <jinalpanchal1897@gmail.com>
2024-05-06 22:59:10 +0530

    added tailwind, created navbar with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'cart', component: CartComponent},
+    {path: 'products', component: ProductsComponent},
+];
