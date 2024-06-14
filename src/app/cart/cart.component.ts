908869a73629822fb4ea3b66df0ab50200ad3064
JinalPanchal-29 <jinalpanchal1897@gmail.com>
2024-06-14 23:31:35 +0530

    update cart quantity

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,28 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonService } from '../services/common.service';
 import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [NgIf,NgFor,FormsModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
+  cart;
+  quantity: Array<number>;
+  
   constructor(
     private commonService: CommonService,
   ){}
-  cart;
+
+  @Input('ngModel')
+  
   ngOnInit(): void {
     this.getCartDetails();
+    this.quantity = Array(10).fill(0).map((x,i)=>{return i+1})
   }
 
   getCartDetails(){
-    this.commonService.getCartDetails()
-      .subscribe((res) => {
-        this.cart = res;
-        console.log(this.cart);
-      });
+    this.commonService.getCartDetails().subscribe((res) => {
+      this.cart = res;
+    });
+  }
+
+  onChangeQunatity(product){
+    console.log(product)
+    let payload = {
+      "productId": product.product._id,
+      "userQuantity":product.userQuantity
+    }
+    console.log(payload)
+    this.commonService.updateCartQuantity(payload).subscribe((res)=>{})
   }
 }
--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -15,16 +15,23 @@ export class CommonService {
 
   addToCart(cartObject:object):Observable<object>{
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJOYW1lIjoiSm9objEyMyIsImVtYWlsSWQiOiJqb2huc21pdGhAZ21haWwuY29tIn0sImlhdCI6MTcxODExNjY0MCwiZXhwIjoxNzE4MTMxMDQwfQ.5VZXMhrHUIik-C0cdCbVTD2LlqeL86uFYTNCqR0wtyw'
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJOYW1lIjoiSm9objEyMyIsImVtYWlsSWQiOiJqb2huc21pdGhAZ21haWwuY29tIn0sImlhdCI6MTcxODM4MDcxMCwiZXhwIjoxNzE4Mzk1MTEwfQ.bvit9Y52wyQ3QDTKdlS46r-OmWqr2lIamgI-shExpQA'
     });
     return this.http.post(`${this.url}common/addtocart`,cartObject,{headers});
   }
 
   getCartDetails(){
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJOYW1lIjoiSm9objEyMyIsImVtYWlsSWQiOiJqb2huc21pdGhAZ21haWwuY29tIn0sImlhdCI6MTcxODExNjY0MCwiZXhwIjoxNzE4MTMxMDQwfQ.5VZXMhrHUIik-C0cdCbVTD2LlqeL86uFYTNCqR0wtyw'
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJOYW1lIjoiSm9objEyMyIsImVtYWlsSWQiOiJqb2huc21pdGhAZ21haWwuY29tIn0sImlhdCI6MTcxODM4MDcxMCwiZXhwIjoxNzE4Mzk1MTEwfQ.bvit9Y52wyQ3QDTKdlS46r-OmWqr2lIamgI-shExpQA'
     });
     return this.http.get(`${this.url}common/getcart`,{headers});
+  }
+
+  updateCartQuantity(payLoad: object){
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJOYW1lIjoiSm9objEyMyIsImVtYWlsSWQiOiJqb2huc21pdGhAZ21haWwuY29tIn0sImlhdCI6MTcxODM4MDcxMCwiZXhwIjoxNzE4Mzk1MTEwfQ.bvit9Y52wyQ3QDTKdlS46r-OmWqr2lIamgI-shExpQA'
+    });
+    return this.http.post(`${this.url}common/updateCartQuantity`,payLoad,{headers});
   }
 
   showToast(message: string): void {
