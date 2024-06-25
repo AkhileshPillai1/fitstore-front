5b407bee7d5313a8a99ce4acef4662e3c618b3b5
Flare49 <officialflare49@gmail.com>
2024-06-25 17:50:53 +0530

    products page enchanced

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,20 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor, NgIf, NgClass } from '@angular/common';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
 import { ProductService } from '../services/product.service';
 import { RouterLink, Router } from '@angular/router';
+import { CommonService } from '../services/common.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [UpperCasePipe,FormsModule,NgFor, NgIf,ProductDetailComponent,RouterLink],
+  imports: [UpperCasePipe, FormsModule, NgFor, NgClass, NgIf, ProductDetailComponent, RouterLink],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-  constructor(private productService: ProductService, private router: Router) {}
+  constructor(private productService: ProductService, private router: Router) { }
+  commonService = inject(CommonService);
+  loaderService = inject(NgxUiLoaderService);
   title = "Fit shop";
   products = [];
   selectedProduct;
@@ -24,13 +28,43 @@ export class ProductsComponent {
   }
 
   getProducts(): void {
-    this.productService.getProducts().subscribe(res=>{
+    this.productService.getProducts().subscribe(res => {
       this.products = res;
     });
   }
 
-  openProductDetail(productId){
+  getBackgroundClass(rating: number) {
+    if (rating >= 4)
+      return 'bg-green';
+    else if (rating >= 2.5)
+      return 'bg-yellow';
+    else
+      return 'bg-red';
+  }
+
+  openProductDetail(productId) {
     this.router.navigate(['/detail/', productId])
+  }
+
+  addToCart(productId) {
+    this.loaderService.start();
+    let cartObject = {
+      productId: productId,
+      quantity: 1
+    };
+    this.commonService.addToCart(cartObject)
+      .subscribe({
+        next: (res) => {
+          if (res["success"]) {
+            this.loaderService.stop();
+            this.commonService.showToast({ message: 'Added to cart!', type: "success", timeout:250});
+          }
+        },
+        error: () => {
+          this.loaderService.stop();
+          this.commonService.showToast({ message: 'Something went wrong, please try again later', type: "error", timeout:250 });
+        }
+      });
   }
 
 }
